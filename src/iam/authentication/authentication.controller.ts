8ee9e9708662ed765e23bf9459a223ca84721491
dome_dexpc <domemacintosh@gmail.com>
2024-08-05 00:05:28 +0700

    init then finish guard

---
+++ src/iam/authentication/authentication.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  // Res,
+} from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { SignUpDto } from './dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto';
+import { Auth } from './decorators/auth.decorator';
+import { AuthType } from './enums/auth-type.enum';
+// import { Response } from 'express';
+
+@Auth(AuthType.None)
+@Controller('authentication')
+export class AuthenticationController {
+  constructor(private readonly authService: AuthenticationService) {}
+
+  @Post('sign-up')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
+  }
+
+  @HttpCode(HttpStatus.OK) // by default @Post does 201, we wanted 200 - hence using @HttpCode(HttpStatus.OK)
+  @Post('sign-in')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
+  }
+
+  // REVIEW: The HTTP only cookie approach
+  /*   @HttpCode(HttpStatus.OK)
+  @Post('sign-in')
+  async signIn(
+    @Res({ passthrough: true }) response: Response,
+    @Body() signInDto: SignInDto,
+  ) {
+    const accessToken = await this.authService.signIn(signInDto);
+    response.cookie('Authentication', accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: true,
+    });
+  } */
+}
